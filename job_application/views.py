59bf98ac11912ba64a3cf263d8d733760bb14f45
Rikuto <mikadorikuto@gmail.com>
2025-12-19 15:33:14 +0900

    update the lecture

--- job_application/views.py
+++ job_application/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def index(request):
+    # request is required to generate HttpResponse and make request data available in templates
     return render(request, "index.html")
