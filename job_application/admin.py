24c71f77f9b149c5d17a1ad3dec139ae7a5bae93
Rikuto <mikadorikuto@gmail.com>
2026-01-02 16:34:30 +0900

    update the code

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+# Import the Form model from the models module in the current package
+from .models import Form
+
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("date", "occupation")
+    # Sort entries by first_name in descending order (Z to A) - the minus sign indicates reverse ordering
+    ordering = ("-first_name",)
+    readonly_fields = ("occupation",)
+
+
+admin.site.register(Form, FormAdmin)
